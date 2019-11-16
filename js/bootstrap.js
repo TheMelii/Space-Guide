8d8452d5053a753e6fb2f03159e4bf91d77090b9
TheMelii <TheMelii@users.noreply.github.com>
2019-11-16 21:49:57 +0000

    changed top navbar at every page try to implement image map again

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -4,6 +4,13 @@
   * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
   */
 
+  $(function() {
+    $('a[href*=#]').on('click', function(e) {
+      e.preventDefault();
+      $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+    });
+  });
+
   $('.multiple-items').indexcarousel({
     infinite: true,
     slidesToShow: 3,
